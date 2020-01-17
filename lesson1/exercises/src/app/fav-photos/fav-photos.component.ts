07e982dc25e3004aadd3d483552698963d6fb258
Joe Crowder <toast.one.art@gmail.com>
2020-01-16 20:24:19 -0800

    rearrang the components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo Title Here';
+  photosTitle = 'Random Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'https://api.time.com/wp-content/uploads/2014/10/tom-hanks-big-movie-poster.jpg?w=600&quality=85';
   image3 = 'https://images-na.ssl-images-amazon.com/images/I/610hrhIQ6KL._AC_.jpg';
