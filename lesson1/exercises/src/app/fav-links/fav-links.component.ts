6560ec9a208b23feb89ad118e0b564ca74b43851
Joe Crowder <toast.one.art@gmail.com>
2020-01-16 20:12:59 -0800

    add more components

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://cheese.com/' , 'https://www.google.com/']
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
