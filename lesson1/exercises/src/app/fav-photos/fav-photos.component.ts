7e4f3226c80901500db5fb09afb87318e0a8effc
Joe Crowder <toast.one.art@gmail.com>
2020-01-16 18:49:13 -0800

    modify the css

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://api.time.com/wp-content/uploads/2014/10/tom-hanks-big-movie-poster.jpg?w=600&quality=85';
+  image3 = 'https://images-na.ssl-images-amazon.com/images/I/610hrhIQ6KL._AC_.jpg';
 
   constructor() { }
 
